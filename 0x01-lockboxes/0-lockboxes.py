7d956a2150866305c0bb1ca9136e1fb3904e96c5
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-15 08:46:49 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+
+    visited = set()
+    queue = [0]  # Start from the first box
+
+    while queue:
+        current_box = queue.pop(0)
+        visited.add(current_box)
+
+        for key in boxes[current_box]:
+            if key not in visited and key < len(boxes):
+                queue.append(key)
+
+    return len(visited) == len(boxes)
+
+# Testing the function
+print(canUnlockAll([[1], [2], [3], [4], []]))  # True
+print(canUnlockAll([[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]))  # True
+print(canUnlockAll([[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]))  # False
+print(canUnlockAll([[0]]))  # True
