794b63c4e42c2f203ca3a2242a4f53a685aee946
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2024-03-25 22:42:36 +0000

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,32 @@
 #!/usr/bin/python3
+"""Locked boxes module"""
+
 
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
+    """Determines if all the boxes can be opened or not
+    Returns:
+        True: if all boxes can be opened
+        False: otherwise
+    """
+    length = len(boxes)
+    keys = set()
+    opened_boxes = []
+    i = 0
 
-    visited = set()
-    queue = [0]  # Start from the first box
+    while i < length:
+        oldi = i
+        opened_boxes.append(i)
+        keys.update(boxes[i])
+        for key in keys:
+            if key != 0 and key < length and key not in opened_boxes:
+                i = key
+                break
+        if oldi != i:
+            continue
+        else:
+            break
 
-    while queue:
-        current_box = queue.pop(0)
-        visited.add(current_box)
-
-        for key in boxes[current_box]:
-            if key not in visited and key < len(boxes):
-                queue.append(key)
-                visited.add(key)  # Add the key to visited
-
-    # Check if all boxes have been visited
-    return len(visited) == len(boxes)
-
-# Testing the function
-print(canUnlockAll([[1], [2], [3], [4], []]))  # True
-print(canUnlockAll([[1, 4, 5], [2], [5, 2], [3], [4, 1], [3, 5]]))  # True
-print(canUnlockAll([[4, 6], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]))  # False
-print(canUnlockAll([[0]])) # True
+    for i in range(length):
+        if i not in opened_boxes and i != 0:
+            return False
+    return True
